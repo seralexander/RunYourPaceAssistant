afd9e003eff068ec3f9e9a5b9385652fe53f3fee
seralexander <122641777+seralexander@users.noreply.github.com>
2025-12-06 19:23:09 +0100

    3 trainings (interval, tempo, duur) voor volgende week

---
+++ Workouts/DEMO.py
@@ -0,0 +1,5 @@
+WORKOUTS = [
+    {"date": "2025-12-09", "name": "Interval 4x3m Z4", "duration_minutes": 55, "description": "Warmup\n- 10m Z1 HR\n\nMain Set\n4x\n- 3m Z4 HR\n- 2m Z2 HR\n\nCooldown\n- 10m Z1 HR"},
+    {"date": "2025-12-10", "name": "Tempo 30m Z3", "duration_minutes": 50, "description": "Warmup\n- 10m Z1 HR\n\nMain Set\n- 30m Z3 HR\n\nCooldown\n- 10m Z1 HR"},
+    {"date": "2025-12-13", "name": "Duurtraining 90m Z2", "duration_minutes": 90, "description": "Warmup\n- 10m Z1 HR\n\nMain Set\n- 70m Z2 HR\n\nCooldown\n- 10m Z1 HR"}
+]
