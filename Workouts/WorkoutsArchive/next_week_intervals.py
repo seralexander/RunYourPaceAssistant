6f4d6a7e5fbfb78c3178496ccc1ab6f6e91589e4
Ser Alexander <alexanderserneels@hotmail.com>
2025-12-07 17:38:46 +0100

    Cleaner gemaakt, geen api meer tussen gpt & RYPA

---
+++ Workouts/WorkoutsArchive/next_week_intervals.py
@@ -0,0 +1,14 @@
+WORKOUTS = [
+  {
+    "date": "2025-12-17",
+    "name": "Intervaltraining 5x3m Z3",
+    "duration_minutes": 60,
+    "description": "Warmup\n- 10m Z1 HR\n\nMain Set\n5x\n- 3m Z3 HR\n- 2m Z2 HR\n\nCooldown\n- 10m Z1 HR"
+  },
+  {
+    "date": "2025-12-19",
+    "name": "Korte Intervals 8x1m Z4",
+    "duration_minutes": 50,
+    "description": "Warmup\n- 10m Z1 HR\n\nMain Set\n8x\n- 1m Z4 HR\n- 2m Z2 HR\n\nCooldown\n- 10m Z1 HR"
+  }
+]
